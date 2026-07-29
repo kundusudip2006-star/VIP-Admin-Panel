98b97dcda20966fafb2dee1beb2d76ecb9c7a71d
kundusudip011-droid <kundusudip011@gmail.com>
2026-07-29 18:18:21 +0530

    Telegram bot setup

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,30 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { onDocumentCreated } = require("firebase-functions/v2/firestore");
+const axios = require("axios");
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const BOT_TOKEN = "8545735365:AAEVzSmQQZiAdznz3FZfyrRXUS5ZPvcqLS4";
+const CHAT_ID = "8152666872";
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+exports.newOrderNotification = onDocumentCreated(
+  "orders/{orderId}",
+  async (event) => {
+    const data = event.data.data();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+    const text = `
+🛒 New Order
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+👤 Name: ${data.name || "N/A"}
+📞 Phone: ${data.phone || "N/A"}
+💰 Amount: ${data.amount || "N/A"}
+`;
+
+    await axios.get(
+      `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+      {
+        params: {
+          chat_id: CHAT_ID,
+          text: text,
+        },
+      }
+    );
+  }
+);
